939d094acc98f46c460e99c2dded1eae553ebf0e
Diego CC <diego.calvo@enreda.coop>
2022-10-10 15:33:53 +0200

    es i ca

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,8 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :ca
-    config.i18n.available_locales = ["ca"]
+    available_locales = [
+      "ca", "es"]
+    config.i18n.available_locales = available_locales
   end
 end
